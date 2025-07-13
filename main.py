93390095fa36a8b830ecdf2369e63b62ed22ec25
Abhinav <raiabhinav.in@gmail.com>
2025-07-13 18:51:04 +0530

    Enhance get_patient endpoint with detailed error handling and input validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException
 import json
 
 app = FastAPI()
@@ -26,10 +26,10 @@ def get_patients():
 
 
 @app.get("/get-patients/{patient_id}")
-def get_patient(patient_id: str):
+def get_patient(patient_id: str = Path(..., description="The ID of the patient to retrieve", example="P001")):
     data = load_data()
 
     if patient_id in data:
         return data[patient_id]
     
-    return {"error": "Patient not found"}
+    raise HTTPException(status_code=404, detail="Patient not found")
